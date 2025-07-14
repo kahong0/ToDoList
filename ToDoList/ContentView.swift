7c6599c0cc84d17aca9c4c1974e634d6ead4a338
Keely Hong <keelyhong06@gmail.com>
2025-07-14 10:25:52 -0400

    added plsu sign shows new view and it also gradually appears (animation)

--- ToDoList/ContentView.swift
+++ ToDoList/ContentView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var showNewTask = false
+    
     var body: some View {
+        
         VStack {
             HStack{
                 Text("To-Do List")
@@ -17,7 +20,9 @@ struct ContentView: View {
                 Spacer()
                 
                 Button{
-                    
+                    withAnimation{
+                        showNewTask = true
+                    }
                 }label: {
                     Text("+")
                         .font(.title)
@@ -27,6 +32,10 @@ struct ContentView: View {
         }
         .padding()
         Spacer()
+        
+        if showNewTask {
+            NewToDoView()
+        }
         
     }
 }
